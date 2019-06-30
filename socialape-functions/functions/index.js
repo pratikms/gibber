0b505d2b523f81ee1dde3ac19f30b3cd8011da10
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2019-07-01 01:01:26 +0530

    Added get and update profile details

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -3,19 +3,21 @@ const functions = require('firebase-functions');
 const express = require('express')
 const app = express()
 
-const FBAuth = require('./utils/fb-auth')
+const FirebaseAuth = require('./utils/fb-auth')
 
 const { getAllScreams, postScream } = require('./handlers/screams')
-const { signup, login, uploadImage } = require('./handlers/users')
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 
 // Scream routes
 app.get('/screams', getAllScreams)
-app.post('/scream', FBAuth, postScream)
+app.post('/scream', FirebaseAuth, postScream)
+app.post('/user/image', FirebaseAuth, uploadImage)
+app.post('/user', FirebaseAuth, addUserDetails)
+app.get('/user', FirebaseAuth, getAuthenticatedUser)
 
 // Users routes
 app.post('/signup', signup)
 app.post('/login', login)
-app.post('/user/image', FBAuth, uploadImage)
 
 exports.api = functions
     .region('asia-east2')
--- socialape-functions/sample-db-schema.js
+++ socialape-functions/sample-db-schema.js
@@ -1,11 +1,46 @@
 let db = {
+    users = [
+        {
+            userId: 'sdf',
+            email: 'user@mail.com',
+            handle: 'user',
+            createdAt: '2019-06-29T19:21:11.256Z',
+            imageUrl: 'image/sdf/sdf',
+            bio: 'Hello',
+            website: 'https://dfsf.com',
+            location: 'India'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
             body: 'this is the scream body',
             createdAt: '2019-06-29T19:21:11.256Z',
             likeCount: 5,
-            commentCount: 2    
+            commentCount: 2
+        }
+    ]
+}
+
+const userDetails = {
+    credentials: {
+        userId: 'sdf',
+        email: 'user@mail.com',
+        handle: 'user',
+        createdAt: '2019-06-29T19:21:11.256Z',
+        imageUrl: 'image/sdf/sdf',
+        bio: 'Hello',
+        website: 'https://dfsf.com',
+        location: 'India'
+    },
+    likes: [
+        {
+            userHandle: 'user',
+            screamId: 'sdflklrter'
+        },
+        {
+            userHandle: 'user',
+            screamId: 'lkjlklrter'
         }
     ]
 }
